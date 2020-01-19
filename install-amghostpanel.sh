e65531b34267c76414c26335b60bd3fb45a92e23
Lirim <42003624+redlineal@users.noreply.github.com>
2020-01-19 17:38:11 +0100

    Update install-amghostpanel.sh

--- install-amghostpanel.sh
+++ install-amghostpanel.sh
@@ -14,7 +14,7 @@ fi
 
 # define version using information from GitHub
 get_latest_release() {
-  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+  curl --silent "https://api.github.com/repos/redlineal/amghost/releases/latest" | # Get latest release from GitHub api
     grep '"tag_name":' |                                            # Get tag line
     sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
 }
